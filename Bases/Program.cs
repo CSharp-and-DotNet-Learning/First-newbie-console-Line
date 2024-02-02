42c136c9d0be8f7e0796ae7872d0dca6b39a3989
dldvlpr <dormoy.dev@gmail.com>
2024-02-02 23:40:30 +0100

    Ajoutez des fichiers projet.

---
+++ Bases/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Bonjour! comment t'appeles-tu ?");
+string? rep = Console.ReadLine();
+
+Console.WriteLine("Bonjour " + rep + " !");
+
+Console.WriteLine("Quelle est ton année de naissance ?");
+rep = Console.ReadLine();
+
+int birthDay = int.Parse(rep);
+
+int age = DateTime.Today.Year - birthDay;
+
+Console.WriteLine($"Tu as ou vas avoir {age} ans en {DateTime.Today.Year}.");
